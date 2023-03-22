e1272c8baf92d89c566f3623915d859e0b60184d
John Spooner <127133215+JSPOON3R@users.noreply.github.com>
2023-03-22 19:22:04 +0100

    22/03

--- surfly.js
+++ surfly.js
@@ -1,78 +1,157 @@
+/* NOTES
+
+SECTIONS:
+STARTING UP - add info as listed
+API BUILDER - create the flow
+EMBED URLS AND BUTTONS - currntly exposing buttons too early. Must update url visuals first
+
+WORKFLOW OF PAGE
+- see info about the embed API
+- click to create an api request
+- hide api request button once it is clicked
+- show api builder: blank call on screen with POST, URL and BODY, who are you box, api key box (info about each input and what it means). Include big 'call api' button on screen but mention this can't be clicked until we hav the right info.
+- let them add their details with inout boxes
+- update API visual the api key added
+- when api is called, display response body and status code
+- when status code - 200, expose the embed section
+- show all the URLs for embed API without access code 
+- text box to enter access code from response
+- update URL visuals with acess code (and the iframes in the back end) / display generate iframe buttons
+- done
+*/
+
+
 
   // STARTING UP: START
 
 // reveal the api builder button start
         function Expose_api_view() {
             document.getElementById('api_builder').style.display = "block";
+
         }
 //reveal the api builder button end
+
 
 //STARTING UP: END
 
 //API BUILDER: START
-//ho are you dropdown start
 
-        /* When the user clicks on the button,
-        toggle between hiding and showing the dropdown content */
-        function dropdown() {
-            document.getElementById("myDropdown").classList.toggle("show");
-        }
+//request body display start
 
-        // Close the dropdown menu if the user clicks outside of it
-        window.onclick = function (event) {
-            if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (openDropdown.classList.contains('show')) {
-                        openDropdown.classList.remove('show');
-                    }
-                }
-            }
-        }
-//who are you dropdown end
+let agent_id = 'EMPTY';
+let email = 'EMPTY';
+let namee = 'EMPTY';
+let role = 'EMPTY';
+let force_new = 'EMPTY';
 
-//store api key entry/update display start
+function request_json(){ 
+
+    document.getElementById("request_body_agent_id").innerHTML = "agent_id: " + agent_id + ",";
+    document.getElementById("request_body_email").innerHTML = "email: " + email + ",";
+    document.getElementById("request_body_name").innerHTML ="name: " + namee + ",";
+    document.getElementById("request_body_role").innerHTML ="role: " + role + ",";
+    document.getElementById("request_body_force_new").innerHTML ="force_new: " + force_new;
+}
+
+
+//request body display end
+
+
+//who are you start
+
+function update_agent_id(){
+        var new_agent_id = document.getElementById("agent_id_input_box").value;
+        agent_id = new_agent_id;
+        document.getElementById("request_body_agent_id").innerHTML = "agent_id: " + agent_id + ",";
+}
+
+function update_email(){
+    var new_email = document.getElementById("email_input_box").value;
+    email = new_email;
+    document.getElementById("request_body_email").innerHTML = "email: " + email + ",";
+
+}
+
+function update_name(){
+    var new_namee = document.getElementById("name_input_box").value;
+    namee = new_namee;
+    document.getElementById("request_body_name").innerHTML ="name: " + namee + ",";
+
+}
+
+function update_role(){
+    var new_role = document.getElementById("role_input_box").value;
+    role = new_role;
+    document.getElementById("request_body_role").innerHTML ="role: " + role + ",";
+
+}
+
+function update_force_new(){
+    var new_force_new = document.getElementById("force_new_input_box").value;
+    force_new = new_force_new;
+    document.getElementById("request_body_force_new").innerHTML ="force_new: " + force_new;
+}
+
+
+
+
+
+
+
+//who are you end
+
+//store api key entry
         function collectApiKey() {
             var key_input = document.getElementById("apikey_input_box").value
             localStorage.setItem('stored_key', key_input)
             console.log("API_KEY submitted: " + key_input)
             console.log("Key stored locally: " + localStorage.getItem('stored_key'))
         }
-//ADD LIST ITEM UPDATE TO THE ABOVE
 
-//Store api key entry/update display end
+//Update Display
 
-
-
-
-
-
-// function to detect both api key and body have been added to request and expose call api button
+function update_api_display(){
+    document.getElementById("api_url_key").innerHTML ="YOUR_SECRET_API_KEY";
+}
 
 
-//call api start-
+
+//call api start
 
         let stored_key = localStorage.getItem('stored_key')
 
         function callAPI() {
-            const response = fetch("https://surfly.com/v2/agents/access-token/?api_key=" + stored_key, {
+//consolidate response into a single variable for API call
+
+
+/* storing values for testing
+321854
+john@surfly.com
+John
+admin
+false
+a69e09988d8d46a1a6b11ad7245eb023
+
+
+https://www.javascripttutorial.net/javascript-fetch-api/
+*/
+            
+
+let request_body = "{\"agent_id\":"+ agent_id+","+"\"email\":"+"\""+email+"\""+","+"\"name\":"+"\""+namee+"\""+","+"\"role\":"+"\""+role+"\""+","+"\"force_new\":"+force_new+"}";
+            console.log("request body log"+request_body);
+
+           fetch("https://surfly.com/v2/agents/access-token/?api_key=" + stored_key, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: `{
-    "agent_id": 321854,
-    "email": "john@surfly.com",
-    "name": "John",
-    "role": "admin",
-    "force_new": false
-}`,
+                body: request_body,
             })
                 .then(response => response.json())
                 .then(response => console.log(JSON.stringify(response)))
+              
+               
 
         }
 //all api end
