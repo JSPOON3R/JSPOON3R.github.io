2c05c44b3eb8b4ae6f405cacf6e367087ad74e97
John Spooner <127133215+JSPOON3R@users.noreply.github.com>
2023-12-01 16:36:26 +0100

    Update masking-visuals-sioa.js

--- script/masking-visuals-sioa.js
+++ script/masking-visuals-sioa.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 let hideSelector; 
-hideSelector = "#InsuredAddress,#HasAcceptedDisclaimer-checkbox-group > label > div > div > span,body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div";
+hideSelector = "#InsuredAddress,#HasAcceptedDisclaimer-checkbox-group > label > div > div > span,body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div,#input_7_3,#input_7_5";
 console.log('Selector masked:', hideSelector);
 
 var cssKeyframes = `
