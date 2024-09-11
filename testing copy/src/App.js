a61ddf08e1eb452f5d5cf9189240c94ad70bfcce
John Spooner <127133215+JSPOON3R@users.noreply.github.com>
2024-09-11 15:58:55 +0200

    app

---
+++ testing copy/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TestSurflyIframe from './TestSurflyIframe'; // Import your component
+
+function App() {
+  return (
+    <div className="App">
+      <TestSurflyIframe />
+    </div>
+  );
+}
+
+export default App;
